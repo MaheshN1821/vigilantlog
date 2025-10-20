3688394170da1f7a932c70839fb89b6f35652aa3
Mahesh <maheshmahi18042004@gmail.com>
2025-10-21 00:45:45 +0530

    vercel changes

--- server.js
+++ server.js
@@ -13,9 +13,4 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
-if (process.env.NODE_ENV !== "production" && process.env.VERCEL !== "1") {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => console.log(`Server listening on ${port}`));
-}
-
 export default app;
