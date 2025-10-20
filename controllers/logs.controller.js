04f6025fff1735be06228a7c1ac0419636ec027b
Mahesh <maheshmahi18042004@gmail.com>
2025-10-20 23:50:07 +0530

    minor changes related to fs

--- controllers/logs.controller.js
+++ controllers/logs.controller.js
@@ -1,10 +1,8 @@
-import fs from "fs";
-
 const partOneController = (req, res) => {
   try {
     const logs = req.body;
     const logsArray = Array.isArray(logs) ? logs : [logs];
-    fs.writeFileSync("logs_part_one.json", JSON.stringify(logsArray, null, 2));
+
     console.log("logs_part_one.json Logs written in flogs_part_one.json");
     res.status(200).send("logs_part_one.json stored!");
   } catch (err) {
@@ -17,7 +15,7 @@ const partTwoController = (req, res) => {
   try {
     const logs = req.body;
     const logsArray = Array.isArray(logs) ? logs : [logs];
-    fs.writeFileSync("logs_part_two.json", JSON.stringify(logsArray, null, 2));
+
     console.log("logs_part_two.json Logs written in logs_part_two.json");
     res.status(200).send("logs_part_two.json Logs stored!");
   } catch (err) {
