6a01493b2e49a15c26493e1e177a602980f6a6af
Mahesh <maheshmahi18042004@gmail.com>
2025-10-22 22:25:08 +0530

    Mongo path corrected

--- server.js
+++ server.js
@@ -18,25 +18,23 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
 });
 
-cron.schedule("* * * * *", async () => {
-  try {
-    await correlateLogs();
-  } catch (err) {
-    console.error("Error during logs correlation:", err);
-  }
-});
-
 const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
   .connect(MONGO_URL)
   .then(() => {
     console.log("Mongo DB is connected!");
+    app.listen(3000, () => {
+      console.log("Server is listening at 3000...");
+    });
+    cron.schedule("* * * * *", async () => {
+      try {
+        await correlateLogs();
+      } catch (err) {
+        console.error("Error during logs correlation:", err);
+      }
+    });
   })
   .catch((err) => {
     console.log(err);
   });
-
-app.listen(3000, () => {
-  console.log("Server is listening at 3000...");
-});
